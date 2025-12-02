f961e6cad2b199788ab9d22157f14976dd98e185
Gregory Daynes <gregdaynes@gmail.com>
2025-12-01 20:02:31 -0800

    refactor(01): minor cleanup
    I need to remember not to tidy too much or it could make future puzzles
    that build on this harder to work with.

--- cmd/day-01/main.go
+++ cmd/day-01/main.go
@@ -10,46 +10,41 @@ import (
 func main() {
 	fmt.Println("Running Day 01")
 
-	start := 50
-	input := ""
-
 	bytes, err := os.ReadFile("./cmd/day-01/input.txt")
 	if err != nil {
 		fmt.Println("error reading input", err)
 		return
 	}
-	input = strings.Trim(string(bytes), "\n")
+	input := strings.Trim(string(bytes), "\n")
 
-	results := Day01(start, input)
-	fmt.Printf("results: %+v", results)
+	results := Day01(50, input)
+	fmt.Printf("results: %+v\n", results)
 }
 
 // a sequence end is marked by rotating to 0
-func Day01(start int, input string) int {
-	seqCount := 0
-	state := start
-
+func Day01(state int, input string) (seqCount int) {
 	for v := range strings.SplitSeq(input, "\n") {
-		mult := 1
-		if dir := v[:1] == "L"; dir == true {
-			mult = -1
+		dir := 1
+		if d := v[:1] == "L"; d == true {
+			dir = -1
 		}
 
 		count, err := strconv.Atoi(v[1:])
 		if err != nil {
-			fmt.Printf("count was not an int, %v received", v)
+			fmt.Printf("count was not an int, %v received\n", v)
 		}
 
 		for range count {
-			state = state + 1*mult
+			state = state + 1*dir
 
 			if state < 0 {
 				state = 99
 			} else if state > 99 {
 				state = 0
 			}
+
 			if state == 0 {
-				seqCount = seqCount + 1
+				seqCount++
 			}
 		}
 	}
